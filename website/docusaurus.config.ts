d09122059f7ac2b01edb90640b06a16a00a578ac
Humbal Ali <humbalali3@gmail.com>
2025-12-06 16:49:13 +0500

    Update baseUrl in docusaurus config for Vercel deployment

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -17,8 +17,8 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://humbalali.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/humaniod-robotics-book/',
+  // For Vercel deployment to root domain
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
